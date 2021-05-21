2698f39a79eb5bb4cacd6e0f63cfe88c86469860
sergiarguimbau <sergiarguimbau@gmail.com>
2021-05-21 20:00:44 +0200

    Debug tools for redux store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,24 @@
 // @see https://github.com/rt2zz/redux-persist (Persisted Store)
+// @see https://github.com/zalmoxisus/redux-devtools-extension (RN Debugger)
+// @see https://github.com/LogRocket/redux-logger (Redux Action Logger)
 
-import { createStore } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
 import { persistStore, persistReducer } from 'redux-persist'
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import { createLogger } from 'redux-logger'
 import rootReducer from './reducer';
+
+const composeEnhancers = composeWithDevTools({});
+
+const logger = createLogger({
+  collapsed: true,
+  predicate: () => __DEV__,
+});
+
+const enhancer = composeEnhancers(
+  applyMiddleware(logger),
+)
 
 const persistConfig = {
   key: 'root',
@@ -12,5 +27,5 @@ const persistConfig = {
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
-export const store = createStore(persistedReducer);
+export const store = createStore(persistedReducer, enhancer);
 export const persistor = persistStore(store);
