f43e7ea833bb2d51c1cac6ecd99c5346c8e5cba5
sergiarguimbau <sergiarguimbau@gmail.com>
2021-05-21 21:19:19 +0200

    Refactor app architecture in modules

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -1,4 +1,10 @@
-// Empty reducer
-const rootReducer = () => {};
+import { combineReducers } from 'redux';
+
+// Import Reducers
+import mainReducer from '../modules/main/MainState';
+
+const rootReducer = combineReducers({
+  main: mainReducer,
+});
 
 export default rootReducer;
